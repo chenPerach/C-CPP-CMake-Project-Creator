1ec88e4e0f1f53bd099985de68ad8eb52bb4a135
chen perach <chen76273@gmail.com>
2021-09-07 00:34:10 +0300

    stupid bug fix

--- src/extension.ts
+++ src/extension.ts
@@ -12,7 +12,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "test" is now active!');
 
-	context.subscriptions.push( vscode.commands.registerCommand('test.createProject', async () => {
+	context.subscriptions.push( vscode.commands.registerCommand('c-cpp-cmake-project-creator.createProject', async () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Generating project');
