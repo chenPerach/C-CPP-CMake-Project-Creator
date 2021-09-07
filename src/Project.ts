506eaeb55dbf4fa7f7c3292919619cccac4d4d3c
chen perach <chen76273@gmail.com>
2021-09-07 15:44:25 +0300

    moved to functional programming

--- src/Project.ts
+++ src/Project.ts
@@ -1,26 +1,22 @@
 import * as vscode from 'vscode';
-import {VSCodeUI} from "./vscode-ui";
+import {getProjectFolder} from "./vscode-ui";
 import * as path from "path";
 import * as fs from "fs-extra";
-import { UserQuery, UserQueryResults } from './user-query';
+import { UserQueryResults } from './user-query';
 
-export class Project {
+export const createProject = async (context : vscode.ExtensionContext, results : UserQueryResults) : Promise<void> => {
 
-    directories: string[] = ['src','include','build','.vscode'];
+    const directories: string[] = ['src','include','build','.vscode'];
 
-    private context: vscode.ExtensionContext;
 
-    constructor(context: vscode.ExtensionContext){
-        this.context = context;
-    }
-    async createFiles(data:UserQueryResults,location:string){
+    const  createFiles = async (data:UserQueryResults,location:string) => {
         try {
-            const tasksPath = path.join(this.context.extensionPath, 'Templates', 'tasks.json');
-            const launchPath = path.join(this.context.extensionPath, 'Templates', 'launch.json');
-            const ignorePath = path.join(this.context.extensionPath, 'Templates', '.gitignore');
+            const tasksPath = path.join(context.extensionPath, 'Templates', 'tasks.json');
+            const launchPath = path.join(context.extensionPath, 'Templates', 'launch.json');
+            const ignorePath = path.join(context.extensionPath, 'Templates', '.gitignore');
 
-            const mainPath = path.join(this.context.extensionPath, 'Templates', data.type, `main.${data.type}`);
-            const cmakefilePath = path.join(this.context.extensionPath, 'Templates', data.type, 'CMakeLists.txt');
+            const mainPath = path.join(context.extensionPath, 'Templates', data.type, `main.${data.type}`);
+            const cmakefilePath = path.join(context.extensionPath, 'Templates', data.type, 'CMakeLists.txt');
             var cmakefile = fs.readFileSync(cmakefilePath, 'utf-8').replace("<name>",data.name).replace("<version>",data.standard);
             var launchfile = fs.readFileSync(launchPath, 'utf-8').replace(/<name>/g,data.name);
             fs.writeFileSync(path.join(location, '.vscode', 'tasks.json'), fs.readFileSync(tasksPath, 'utf-8'));
@@ -34,23 +30,20 @@ export class Project {
         } catch (err) {
             console.error(err);
         }
-    }
-    async createFolders(location :string){
-        this.directories.forEach((dir: string) => {
+    };
+    const  createFolders = async (location :string) => {
+        directories.forEach((dir: string) => {
             try {
                 fs.ensureDirSync(path.join(location, dir));
             } catch (err) {
                 console.error(err);
             }
         });
+    };
+    const location : vscode.Uri = await getProjectFolder();
+    if(location && location.fsPath){
+        await vscode.commands.executeCommand('vscode.openFolder',location);
+        await createFolders(location.fsPath);
+        await createFiles(results,location.fsPath);
     }
-    async createProject(data:UserQueryResults){
-        const location : vscode.Uri = await VSCodeUI.openDialogForFolder();
-        if(location && location.fsPath){
-            await vscode.commands.executeCommand('vscode.openFolder',location);
-            await this.createFolders(location.fsPath);
-            await this.createFiles(data,location.fsPath);
-        }
-
-    }
-}
+};
